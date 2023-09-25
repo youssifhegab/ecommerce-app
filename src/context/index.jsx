674196972997c0a3db617dcf71540dc30faaf691
youssifhegab <youssif.hegab@zyda.com>
2023-09-26 01:19:11 +0300

    Remove toaster from context provider

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -2,7 +2,6 @@
 
 import { CartProvider } from 'use-shopping-cart';
 
-import { Toaster } from '@/components/UIComponents/Toaster';
 import { TailwindIndicator } from '@/components/TailwindIndicator';
 import GlobalStateProvider from './GlobalState';
 import { ThemeProvider } from './ThemeProvider';
@@ -12,7 +11,6 @@ export function Providers({ children }) {
     <CartProvider currency="USD" shouldPersist cartMode="checkout-session" stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <GlobalStateProvider>
-          <Toaster />
           {children}
           <TailwindIndicator />
         </GlobalStateProvider>
